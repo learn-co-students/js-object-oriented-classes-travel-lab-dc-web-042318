91b19ef3cdf86f39094496a7543a9c1b04dc87cc
Adam Cohn <adamcohn88@gmail.com>
2018-06-08 16:25:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+function nycBlocks() {
+  return {
+    '1st Avenue': 1,
+    '2nd Avenue': 2,
+    '3rd Avenue': 3,
+    'Lexington Avenue': 4,
+    Park: 5,
+    'Madison Avenue': 6,
+    '5th Avenue': 7,
+  };
+}
 
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+
+  yearsExperienceFromBeginningOf() {
+    return new Date().getFullYear() - this.startDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    const verticalBlocks = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical);
+    const horizontalBlocks = Math.abs(nycBlocks()[this.endingLocation.horizontal] - nycBlocks()[this.beginningLocation.horizontal]);
+    return verticalBlocks + horizontalBlocks;
+  }
+
+  estimatedTime(isPeak) {
+    return isPeak ? this.blocksTravelled() / 2 : this.blocksTravelled() / 3;
+  }
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,58 +1,72 @@
 const expect = chai.expect;
 
-describe('driver', function() {
+describe('driver', () => {
   let driver;
   before(() => {
-    driver = new Driver("Alfie", "Aug 9, 1995")
-  })
+    driver = new Driver('Alfie', 'Aug 9, 1995');
+  });
 
-  describe('creating a new driver', function() {
-    it('can create a Driver with a name and startDate', function() {
-      expect(driver.name).to.equal("Alfie")
-    })
+  describe('creating a new driver', () => {
+    it('can create a Driver with a name and startDate', () => {
+      expect(driver.name).to.equal('Alfie');
+    });
 
-    it('sets the driver startDate as a date object', function() {
-      expect(driver.startDate).to.be.instanceof(Date)
-    })
-  })
+    it('sets the driver startDate as a date object', () => {
+      expect(driver.startDate).to.be.instanceof(Date);
+    });
+  });
 
-  describe('yearsExperienceFromBeginningOf', function() {
-    it('calculates the number of years driven given an endDate', function() {
-      expect(driver.yearsExperienceFromBeginningOf(2017)).to.equal(22)
-    })
-  })
-})
+  describe('yearsExperienceFromBeginningOf', () => {
+    it('calculates the number of years driven given an endDate', () => {
+      expect(driver.yearsExperienceFromBeginningOf(2018)).to.equal(23);
+    });
+  });
+});
 
-
-describe('Route', function() {
+describe('Route', () => {
   let route;
 
-  describe('blocksTravelled', function() {
-    it('calculates the number of blocksTravelled', function() {
-      let route = new Route({horizontal: 'Park', vertical: '34'}, {horizontal: 'Park', vertical: '45'})
-      expect(route.blocksTravelled()).to.equal(11)
-    })
+  describe('blocksTravelled', () => {
+    it('calculates the number of blocksTravelled', () => {
+      const route = new Route(
+        { horizontal: 'Park', vertical: '34' },
+        { horizontal: 'Park', vertical: '45' },
+      );
+      expect(route.blocksTravelled()).to.equal(11);
+    });
 
-    it('calculates the number of horizontal blocks travelled', function() {
-      let route = new Route({horizontal: '1st Avenue', vertical: '34'}, {horizontal: 'Park', vertical: '34'})
-      expect(route.blocksTravelled()).to.equal(4)
-    })
+    it('calculates the number of horizontal blocks travelled', () => {
+      const route = new Route(
+        { horizontal: '1st Avenue', vertical: '34' },
+        { horizontal: 'Park', vertical: '34' },
+      );
+      expect(route.blocksTravelled()).to.equal(4);
+    });
 
-    it('combines horizontal and vertical blocks travelled', function() {
-      let route = new Route({horizontal: '1st Avenue', vertical: '34'}, {horizontal: 'Park', vertical: '45'})
-      expect(route.blocksTravelled()).to.equal(15)
-    })
-  })
+    it('combines horizontal and vertical blocks travelled', () => {
+      const route = new Route(
+        { horizontal: '1st Avenue', vertical: '34' },
+        { horizontal: 'Park', vertical: '45' },
+      );
+      expect(route.blocksTravelled()).to.equal(15);
+    });
+  });
 
-  describe('estimatedTime', function() {
-    it('estimates time in minutes, with travel time of three blocks in a minute', function() {
-      let route = new Route({horizontal: '1st Avenue', vertical: '34'}, {horizontal: 'Park', vertical: '45'})
-      expect(route.estimatedTime()).to.equal(5)
-    })
+  describe('estimatedTime', () => {
+    it('estimates time in minutes, with travel time of three blocks in a minute', () => {
+      const route = new Route(
+        { horizontal: '1st Avenue', vertical: '34' },
+        { horizontal: 'Park', vertical: '45' },
+      );
+      expect(route.estimatedTime()).to.equal(5);
+    });
 
-    it('estimates time in minutes, with travel time of two blocks in a minute during peak hours', function() {
-      let route = new Route({horizontal: '1st Avenue', vertical: '34'}, {horizontal: 'Park', vertical: '46'})
-      expect(route.estimatedTime(true)).to.equal(8)
-    })
-  })
-})
+    it('estimates time in minutes, with travel time of two blocks in a minute during peak hours', () => {
+      const route = new Route(
+        { horizontal: '1st Avenue', vertical: '34' },
+        { horizontal: 'Park', vertical: '46' },
+      );
+      expect(route.estimatedTime(true)).to.equal(8);
+    });
+  });
+});
